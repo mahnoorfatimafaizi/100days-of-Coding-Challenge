3d026442d2aa48e90205184fedcc0ea2ae67eeec
mahnoorfatima2002 <mahnoorf2002@gmail.com>
2024-07-01 19:28:02 +0500

    TypeScript

---
+++ Day24/blockScopeWithLetAndConst/index.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+{
+    let blockLet = "visible inside the block";
+    const blockConst = "also only inside the block";
+    console.log(blockLet);
+    console.log(blockConst);
+}
+try {
+    console.log(blockLet);
+}
+catch (error) {
+    console.log("`blockLet` is not accessible outside the block.");
+}
+try {
+    console.log(blockConst);
+}
+catch (error) {
+    console.log("`blockConst` is not accessible outside the block.");
+}
