84605804b975b62372659a21a847464eaa808041
mahnoorfatima2002 <mahnoorf2002@gmail.com>
2024-07-01 17:22:52 +0500

    TypeScript

---
+++ Day21/shapeShifter/index.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let circle = {
+    kind: "circle",
+    radius: 5,
+};
+let rectangle = {
+    kind: "rectangle",
+    width: 10,
+    height: 20,
+};
+console.log(circle);
+console.log(rectangle);
