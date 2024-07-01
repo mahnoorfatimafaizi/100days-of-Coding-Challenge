dfd104901b0cc94612001af6236ab2a475781bbb
mahnoorfatima2002 <mahnoorf2002@gmail.com>
2024-07-01 18:00:12 +0500

    TypeScript

---
+++ Day23/arithmeticWithMixedTypes/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function addNumberAndString(number1, numberString) {
+    return number1 + Number(numberString);
+}
+console.log(addNumberAndString(10, "5"));
---
+++ Day23/arithmeticWithMixedTypes/index.ts
@@ -0,0 +1,5 @@
+function addNumberAndString(number1: number, numberString: string): number {
+    return number1 + Number(numberString);
+}
+
+console.log(addNumberAndString(10, "5"));
