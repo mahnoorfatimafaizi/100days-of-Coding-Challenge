2e0c956ebf49c9dc165231100c48a456de9bd41d
mahnoorfatima2002 <mahnoorf2002@gmail.com>
2024-07-01 19:12:54 +0500

    TypeScript

---
+++ Day24/understandingLetInLoops/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function printNumbersWithLet() {
+    for (let i = 1; i <= 5; i++) {
+        console.log(i);
+    }
+}
+printNumbersWithLet();
---
+++ Day24/understandingLetInLoops/index.ts
@@ -0,0 +1,9 @@
+function printNumbersWithLet() {
+    for (let i = 1; i <= 5; i++) {
+      
+      console.log(i); 
+    }
+    
+  }
+  
+  printNumbersWithLet();
